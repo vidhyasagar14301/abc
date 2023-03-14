951492c5474c7f4b6baf4bbe7105b18fec46f138
vidhyasagar14301 <127750396+vidhyasagar14301@users.noreply.github.com>
2023-03-14 11:32:15 +0530

    first program

---
+++ opencart/src/opencartpackage/TastClass.java
@@ -0,0 +1,11 @@
+package opencartpackage;
+
+public class TastClass {
+
+	public static void main(String[] args) {
+		System.out.println("First Program");
+		System.out.println("Test Class");
+
+	}
+
+}
