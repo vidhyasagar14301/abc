d15eeafca4eabc204efe3dfca8647261b0a9fe66
vidhyasagar14301 <127750396+vidhyasagar14301@users.noreply.github.com>
2023-03-24 20:56:38 +0530

    Update TastClass.java

--- opencart/src/opencartpackage/TastClass.java
+++ opencart/src/opencartpackage/TastClass.java
@@ -5,7 +5,7 @@ public class TastClass {
 	public static void main(String[] args) {
 		System.out.println("First Program");
 		System.out.println("Test Class");
-
+System.out.println("Test Class");
 	}
 
 }
